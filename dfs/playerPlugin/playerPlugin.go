53f53637ea1abb0e80dad9d198009ae48e78018b
vivianzhang <jzhang230@usfca.edu>
2022-11-28 19:02:02 -0800

    update custom MapReduce job

--- dfs/playerPlugin/playerPlugin.go
+++ dfs/playerPlugin/playerPlugin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"hash/fnv"
+	"math"
 	"sort"
 	"strconv"
 	"strings"
@@ -9,6 +10,8 @@ import (
 
 // on VScode: go build -buildmode=plugin -gcflags="all=-N -l" -o ./playerPluginrun/playerPlugin.so ./playerPlugin/playerPlugin.go
 // on orions: go build -buildmode=plugin -o ./playerPluginrun/playerPlugin.so ./playerPlugin/playerPlugin.go
+
+//map: read data and add each playerName-score pair into a map, then return a list of maps. Each map only contains one playerName-score pair.
 func Map(line_number int, line_text string) []*map[string]uint32 {
 	var mapped []*map[string]uint32
 	words := strings.Split(line_text, ",")
@@ -24,6 +27,8 @@ func Map(line_number int, line_text string) []*map[string]uint32 {
 	return mapped
 }
 
+//shuffle: takes map's result as input, shuffle data by players' name's hash value. Then distribute data to different reducers.
+//         Output is a map, which key is the reducers' index and value is a list of maps, like map phrase, each map contains one playerName-score pair.
 func Shuffle(mappedList []*map[string]uint32, reducernum uint32) map[int]([]*map[string]uint32) {
 	shuffled := make(map[int]([]*map[string]uint32))
 	for i := 0; i < int(reducernum); i++ {
@@ -42,6 +47,7 @@ func Shuffle(mappedList []*map[string]uint32, reducernum uint32) map[int]([]*map
 	return shuffled
 }
 
+//sort: takes shuffle's output as input, output is map, which key is player's name and value is a list of the corresponding player's scores.
 func Sort(shuffledList []*map[string]uint32) []*map[string][]uint32 {
 	grouped := make(map[string][]uint32)
 	for _, shuffled := range shuffledList {
@@ -68,16 +74,20 @@ func Sort(shuffledList []*map[string]uint32) []*map[string][]uint32 {
 	return sorted
 }
 
+//reduce: takes the output of the sort phrase as input, and then calculates the total-score and average-score per game for each player.
+//        So the output is a list of maps, the key is players' name and the value is the PTS（Points Per Game） of the corresponding player.
 func Reduce(sorted []*map[string][]uint32) []*map[string]uint32 {
 	reduced := []*map[string]uint32{}
 	for _, m := range sorted {
 		for key, value := range *m {
 			totalscore := uint32(0)
+			games := len(value)
 			for _, score := range value {
 				totalscore += score
 			}
+			averageScore := (math.Round(float64((int(totalscore) / games)* 100)) / 100)
 			temp := make(map[string]uint32)
-			temp[key] = totalscore
+			temp[key] = uint32(averageScore)
 			reduced = append(reduced, &temp)
 		}
 	}
